a851da2b90a425bb39ddfc630c9819e6d38f248d
dev-jv <dev.javierleon@gmail.com>
2021-01-05 00:27:47 -0500

    six

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -5,4 +5,10 @@ import {Component} from '@angular/core';
   templateUrl: './body.component.html'
 })
 export class BodyComponent {
+  view = true;
+  phr: any = {
+    mss: 'When you open your mind to the impossible, sometimes you find the truth.',
+    aut: 'W.B.'
+  };
+  pers: string[] = ['William Bell', 'Olivia Dunham', 'Peter Bishop', 'Henrietta Bishop', 'Astrid Farnsworth'];
 }
